a8313c5a09e5fb4f11ddc7e0598c48f257383c52
Evedery20 <evedery20@gmail.com>
2023-05-17 16:18:23 -0700

    California

---
+++ 0x0E-SQL_more_queries/8-cities_of_california_subquery.sql
@@ -0,0 +1,4 @@
+-- 0x0E. SQL - More queries, task 8. Cities of California
+-- Lists all cities in CA found in `hbtn_0d_usa`. DB in args.
+SELECT id, name FROM cities WHERE state_id = (SELECT id FROM states WHERE name = 'California') ORDER BY id ASC;
+
