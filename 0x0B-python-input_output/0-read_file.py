33ce40b2521525b0c154b75b34eae3c776b48654
Evedery20 <evedery20@gmail.com>
2023-05-16 22:49:14 -0700

    read file

---
+++ 0x0B-python-input_output/0-read_file.py
@@ -0,0 +1,7 @@
+"""Defines a function for reading text files."""
+
+def read_file(filename=""):
+"""Prints the contents of a UTF8 text file to the standard output."""
+with open(filename, encoding="utf-8") as f:
+print(f.read(), end="")
+
