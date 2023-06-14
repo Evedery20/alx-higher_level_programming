e433d76b4a0fd97e116e208431eb6a38bd204188
Evedery20 <evedery20@gmail.com>
2023-06-14 08:05:30 -0700

    Javascript warm up

---
+++ 0x12-javascript-warm_up/100-let_me_const.js
@@ -0,0 +1,4 @@
+#!/usr/bin/node
+// modifies value of myVar (assigned elsewhere w/o delaration = global var)
+myVar = 333;
+
---
+++ 0x12-javascript-warm_up/6-multi_languages_loop.js
@@ -0,0 +1,7 @@
+#!/usr/bin/node
+// loop through 3 messages to print to console
+const msgArray = ['C is fun', 'Python is cool', 'Javascript is amazing'];
+for (let i = 0; i < 3; i++) {
+  console.log(msgArray[i]);
+}
+
