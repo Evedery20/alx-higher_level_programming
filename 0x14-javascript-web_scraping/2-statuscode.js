736c20e162776361ae11311e01474cd5300faff6
Evedery20 <evedery20@gmail.com>
2023-07-25 21:43:34 +0000

    Web

---
+++ 0x14-javascript-web_scraping/2-statuscode.js
@@ -0,0 +1,12 @@
+#!/usr/bin/node
+
+// displays response status code of a GET request
+const request = require('request');
+
+request(process.argv[2], function (error, response, body) {
+  if (error) {
+    console.error(error);
+  }
+  console.log('code:', response && response.statusCode);
+});
+
